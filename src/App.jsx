ebb13c2d64f37bd16ea7ea28a8f6ac911737c0a1
Mahdi86ir <mohammadmahdi86ir@gmail.com>
2025-05-27 14:16:37 +0330

    fix: posts responsive

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,8 @@ export default function App() {
       <Subscribe/>
       <Curated/>
       <BlogPosts/>
-      {/* <Contact/>
-      <Footer/> */}
+      <Contact/>
+      <Footer/>
     </main>
   )
 }
