89f4ba18ce682ccf5c18075efd270b6c8b17e599
Mahdi86ir <mohammadmahdi86ir@gmail.com>
2025-08-03 21:28:52 +0330

    fix: update 3

--- src/Components/Arts/index.jsx
+++ src/Components/Arts/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Art from '../Art'
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -14,6 +14,8 @@ import { EffectCoverflow, Pagination , Autoplay } from 'swiper/modules';
 export default function Arts({ imgsContainer }) {
   const [clicked , setClicked] = useState(false)
   const [clickedImg , setClickedImg] = useState("")
+  const swiperRef = useRef(null)
+
   const showImage = (src) => {
     setClickedImg(src)
   }
@@ -28,73 +30,52 @@ export default function Arts({ imgsContainer }) {
       setClicked(true)
     }
   },[clickedImg])
+useEffect(() => {
+  if (swiperRef.current) {
+    if (clicked) {
+      swiperRef.current.autoplay.stop()
+    } else {
+      swiperRef.current.autoplay.start()
+    }
+  }
+}, [clicked])
 
   
   return (
     <section className="arts_container">
-           <Swiper
-        effect={'coverflow'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        autoplay={{
-          delay: 2000,
-          disableOnInteraction: false,
-        }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination , Autoplay]}
-        className="mySwiper"
-      >
+ <Swiper
+  effect={'coverflow'}
+  grabCursor={true}
+  centeredSlides={true}
+  slidesPerView={'auto'}
+  coverflowEffect={{
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  }}
+  autoplay={{
+    delay: 2000,
+    disableOnInteraction: false,
+  }}
+  pagination={true}
+  modules={[EffectCoverflow, Pagination, Autoplay]}
+  className="mySwiper"
+  onSwiper={(swiper) => (swiperRef.current = swiper)}
+>
+
         {
           imgsContainer.map(item => (
-            <SwiperSlide key={item}>
+            <SwiperSlide key={item} onClick={() => showImage(item)}>
               <img src={item} />
             </SwiperSlide>
           ))
         }
-        {/* <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-        </SwiperSlide> */}
       </Swiper>
-        {/* <div className={style.content}>
-          {
-            imgsContainer.map(item => <Art clickFunc={showImage} key={item} imgSrc={item} />)
-          }
-        </div>
         {
           clicked && 
-          <div className={style.image_panel}>
+          <div className='image_panel'>
             <div>
               <button type="button" onClick={closeImg}>
                 <i className='bx bx-x'></i>
@@ -102,7 +83,7 @@ export default function Arts({ imgsContainer }) {
             </div>
             <img src={clickedImg} alt="" />
           </div>
-        } */}
+        }
     </section>
   )
 }
